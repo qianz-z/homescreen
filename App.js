7a8c9fd2678d04e56ae64b2de02390fb01775ce6
qianz-z <qianz321@gmail.com>
2021-06-15 16:10:27 +0800

    hawkerfeed not showing

--- App.js
+++ App.js
@@ -6,154 +6,184 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Swipeable from "react-native-gesture-handler/Swipeable";
 
 function RecipeScreen() {
-    return (
-        <View style={styles.container}>
-            <Text>Recipe Page</Text>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Text>Recipe Page</Text>
+    </View>
+  )
 }
 
 
 function RecipefeedScreen({ navigation }) {
 
-    const [vids, newvids] = useState([
-        { key: '1', uri: 'https://miro.medium.com/max/1838/0*bX1mygYPmp4pyypU.png' },
-        { key: '2', uri: 'https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/turner-36_s1-Full-Image_GalleryBackground-en-US-1557356293721._SX1080_.jpg' },
-        { key: '3', uri: 'https://fsa.zobj.net/crop.php?r=7_BFbnFYGNjozHjg-1LKtqqVvd38zFtetigXUN0Au353kpdYkBDh2SHIpQ_FrU2ABBE86NMHtxFwP1t2WGyT_68HOjUQx9vgF-060ZwpUPm90jY1wPRDEY9Bb6ByVVsIqiPMZPHxKklqO1E3' }
-    ]);
+  const [vids, newvids] = useState([
+    { key: '1', uri: 'https://miro.medium.com/max/1838/0*bX1mygYPmp4pyypU.png' },
+    { key: '2', uri: 'https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/turner-36_s1-Full-Image_GalleryBackground-en-US-1557356293721._SX1080_.jpg' },
+    { key: '3', uri: 'https://fsa.zobj.net/crop.php?r=7_BFbnFYGNjozHjg-1LKtqqVvd38zFtetigXUN0Au353kpdYkBDh2SHIpQ_FrU2ABBE86NMHtxFwP1t2WGyT_68HOjUQx9vgF-060ZwpUPm90jY1wPRDEY9Bb6ByVVsIqiPMZPHxKklqO1E3' }
+  ]);
 
-    const leftswipe = () => {
-        return (
-            <View style={styles.leftswipetext}>
-                <Text>Bookmark</Text>
-            </View>
-        )
-    }
-
-    function renderItem({ item }) { //takes an item from data and renders it into the list
-        return (
-            <Swipeable
-                renderLeftActions={leftswipe}
-                renderRightActions={RecipeScreen}>
-                <View style={styles.container}>
-                    <Image style={styles.image} source={{ uri: item.uri }} />
-                </View>
-            </Swipeable >
-        );
-    }
-
+  const leftswipe = () => {
     return (
-        <SafeAreaView style={styles.container}>
-            <Text style={{top:50, zIndex:100, backgroundColor:"transparent", position:"absolute"}} onPress={() => navigation.navigate("Hawkerfeed")}>Tap to go to Hawkerfeed</Text>
-            <FlatList
-                data={vids}
-                renderItem={renderItem}
-                snapToAlignment={'top'}
-                snapToInterval={Dimensions.get('screen').height}
-                pagingEnabled={true}
-                decelerationRate={'fast'} />
-        </SafeAreaView>
+      <View style={styles.leftswipetext}>
+        <Text>Bookmark</Text>
+      </View>
     )
+  }
+
+  function renderItem({ item }) { //takes an item from data and renders it into the list
+    return (
+      <Swipeable
+        renderLeftActions={leftswipe}
+        renderRightActions={RecipeScreen}>
+        <View style={styles.container}>
+          <Image style={styles.image} source={{ uri: item.uri }} />
+        </View>
+      </Swipeable >
+    );
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={{ top: 50, zIndex: 100, backgroundColor: "transparent", position: "absolute" }} onPress={() => navigation.navigate("Hawkerfeed")}>Tap to go to Hawkerfeed</Text>
+      <FlatList
+        data={vids}
+        renderItem={renderItem}
+        snapToAlignment={'top'}
+        snapToInterval={Dimensions.get('screen').height}
+        pagingEnabled={true}
+        decelerationRate={'fast'} />
+    </SafeAreaView>
+  )
 }
 
 function HawkerfeedScreen({ navigation }) {
 
+  const [vids1, newvids1] = useState([
+    { key: '1', uri: 'https://miro.medium.com/max/1838/0*bX1mygYPmp4pyypU.png' },
+    { key: '2', uri: 'https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/turner-36_s1-Full-Image_GalleryBackground-en-US-1557356293721._SX1080_.jpg' },
+    { key: '3', uri: 'https://fsa.zobj.net/crop.php?r=7_BFbnFYGNjozHjg-1LKtqqVvd38zFtetigXUN0Au353kpdYkBDh2SHIpQ_FrU2ABBE86NMHtxFwP1t2WGyT_68HOjUQx9vgF-060ZwpUPm90jY1wPRDEY9Bb6ByVVsIqiPMZPHxKklqO1E3' }
+  ]);
+
+  const leftswipe = () => {
     return (
-        <SafeAreaView style={styles.container}>
-            <Text style={{top:50, zIndex:100, backgroundColor:"transparent", position:"absolute"}} onPress={() => navigation.navigate("Recipefeed")}>Tap to go to Recipefeed</Text>
-            <Text>Hawkerfeed Page</Text>
-        </SafeAreaView>
+      <View style={styles.leftswipetext}>
+        <Text>Bookmark</Text>
+      </View>
     )
+  }
+
+  function renderItem({ item }) { //takes an item from data and renders it into the list
+    return (
+      <Swipeable
+        renderLeftActions={leftswipe}
+        renderRightActions={HawkerScreen}>
+        <View style={styles.container}>
+          <Image style={styles.image} source={{ uri: item.uri }} />
+        </View>
+      </Swipeable >
+    );
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.filter} onPress={() => navigation.navigate("Recipefeed")}>Tap to go to Recipefeed</Text>
+      <Text>Hawkerfeed Page</Text>
+    </SafeAreaView>
+  )
+
+
 }
 
 function HawkerfeedstackScreen() {
-    const Hawkerfeedstack = createStackNavigator();
-    return (
-        <Hawkerfeedstack.Navigator>
-            <Hawkerfeedstack.Screen
-                name="Hawkerfeed"
-                component={HawkerfeedScreen}
-            />
-        </Hawkerfeedstack.Navigator>
-    );
+  const Hawkerfeedstack = createStackNavigator();
+  return (
+    <Hawkerfeedstack.Navigator>
+      <Hawkerfeedstack.Screen
+        name="Hawkerfeed"
+        component={HawkerfeedScreen}
+      />
+    </Hawkerfeedstack.Navigator>
+  );
 }
 
 function RecipefeedstackScreen() {
-    const Recipefeedstack = createStackNavigator();
-    return (
-        <Recipefeedstack.Navigator
-            screenOptions={{
-                headerShown: false
-            }}>
-            <Recipefeedstack.Screen
-                name="Recipefeed"
-                component={RecipefeedScreen}
-                options={{
-                    headerTitle: "Recipefeed",
-                    headerTitleStyle: styles.headerTitleStyle,
-                    headerStyle: styles.headerStyle,
-                }}
-            />
-        </Recipefeedstack.Navigator>
-    );
+  const Recipefeedstack = createStackNavigator();
+  return (
+    <Recipefeedstack.Navigator
+      screenOptions={{
+        headerShown: false
+      }}>
+      <Recipefeedstack.Screen
+        name="Recipefeed"
+        component={RecipefeedScreen}
+        options={{
+          headerTitle: "Recipefeed",
+          headerTitleStyle: styles.headerTitleStyle,
+          headerStyle: styles.headerStyle,
+        }}
+      />
+    </Recipefeedstack.Navigator>
+  );
 }
 
 const Stack = createStackNavigator();
 export default function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator mode="modal" headerMode="none">
-                <Stack.Screen name="Recipefeedstack" component={RecipefeedstackScreen} />
+  return (
+    <NavigationContainer>
+      <Stack.Navigator mode="modal" headerMode="none">
+        <Stack.Screen name="Recipefeedstack" component={RecipefeedstackScreen} />
 
-                <Stack.Screen
-                    name="Recipe"
-                    component={RecipeScreen}
-                    options={{
-                        headerTitle: "Recipe",
-                        headerTitleStyle: styles.headerTitleStyle,
-                        headerStyle: styles.headerStyle,
-                    }} />
+        <Stack.Screen
+          name="Recipe"
+          component={RecipeScreen}
+          options={{
+            headerTitle: "Recipe",
+            headerTitleStyle: styles.headerTitleStyle,
+            headerStyle: styles.headerStyle,
+          }} />
 
-                <Stack.Screen name="Hawkerfeedstack" component={HawkerfeedstackScreen} />
-                <Stack.Screen
-                    name="Hawkerfeed"
-                    component={HawkerfeedScreen}
-                    options={{
-                        headerTitle: "Hawkerfeed",
-                        headerTitleStyle: styles.headerTitleStyle,
-                        headerStyle: styles.headerStyle,
-                    }}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+        <Stack.Screen name="Hawkerfeedstack" component={HawkerfeedstackScreen} />
+        <Stack.Screen
+          name="Hawkerfeed"
+          component={HawkerfeedScreen}
+          options={{
+            headerTitle: "Hawkerfeed",
+            headerTitleStyle: styles.headerTitleStyle,
+            headerStyle: styles.headerStyle,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#cdd",
-    },
-    leftswipetext: {
-        fontSize: 30,
-        backgroundColor: 'transparent',
-        alignItems: "center",
-        justifyContent: "center",
-        width: 80,
-    },
-    image: {
-        height: Dimensions.get('window').height,
-        width: Dimensions.get('window').width,
-        flex: 1
-    },
-    filter: {
-        backgroundColor: 'transparent',
-        alignItems: "center",
-        justifyContent: "center",
-        height: 60,
-        width: 60,
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#cdd",
+  },
+  leftswipetext: {
+    fontSize: 30,
+    backgroundColor: 'transparent',
+    alignItems: "center",
+    justifyContent: "center",
+    width: 80,
+  },
+  image: {
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
+    flex: 1
+  },
+  filter: {
+    backgroundColor: 'transparent',
+    alignItems: "center",
+    justifyContent: "center",
+    height: 60,
+    width: 60,
+    position: 'absolute',
+    top: 100
+  }
 });
